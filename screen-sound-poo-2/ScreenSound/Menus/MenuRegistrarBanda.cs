70f2f4e17ec99f6b4035c943e89b4e3e36539753
Pedro Dias <pedrocorchogdev@gmail.com>
2025-06-09 16:49:56 -0300

    feat: add OOP-2 educational project

---
+++ screen-sound-poo-2/ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -0,0 +1,32 @@
+﻿using OpenAI_API;
+using ScreenSound.Modelos;
+using OpenAI_API;
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new Banda(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
+
+        var client = new OpenAIAPI("api-key");
+
+        var chat = client.Chat.CreateConversation();
+
+        chat.AppendSystemMessage("Resuma a banda The Beatles em 1 parágrafo. Aodte um estilo informal.");
+        string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        Console.WriteLine(resposta);
+
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+    }
+
+}
---
+++ screen-sound-poo-2/ScreenSound/Program.cs
@@ -0,0 +1,70 @@
+﻿using ScreenSound.Menus;
+using ScreenSound.Modelos;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {    
+        Banda banda1 = new Banda("Linkin Park");
+        banda1.AdicionarNota(new Avaliacao(10));
+        banda1.AdicionarNota(new Avaliacao(9));
+        banda1.AdicionarNota(new Avaliacao(8));
+
+        Banda beatles = new("The Beatles");
+
+        Dictionary<string, Banda> bandasRegistradas = new();
+        bandasRegistradas.Add(banda1.Nome, banda1);
+        bandasRegistradas.Add("The Beatles", beatles);
+
+        Dictionary<int, Menu> opcoes = new();
+        opcoes.Add(1, new MenuRegistrarBanda());
+        opcoes.Add(2, new MenuRegistrarAlbum());
+        opcoes.Add(3, new MenuExibirBandasRegistradas());
+        opcoes.Add(4, new MenuAvaliarBanda());
+        opcoes.Add(5, new MenuAvaliarAlbum());
+        opcoes.Add(6, new MenuExibirDetalhes());
+        opcoes.Add(-1, new MenuSair());
+
+        void ExibirLogo()
+        {
+            Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+            Console.WriteLine("Boas vindas ao Screen Sound 2.0!");
+        }
+
+        void ExibirOpcoesDoMenu()
+        {
+            ExibirLogo();
+            Console.WriteLine("\nDigite 1 para registrar uma banda");
+            Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
+            Console.WriteLine("Digite 3 para mostrar todas as bandas");
+            Console.WriteLine("Digite 4 para avaliar uma banda");
+            Console.WriteLine("Digite 5 para avaliar um álbum");
+            Console.WriteLine("Digite 6 para exibir os detalhes de uma banda");
+            Console.WriteLine("Digite -1 para sair");
+
+            Console.Write("\nDigite a sua opção: ");
+            string opcaoEscolhida = Console.ReadLine()!;
+            int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+            if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
+            {
+                Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
+                menuASerExibido.Executar(bandasRegistradas);
+
+                if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
+            }
+            else {
+                Console.WriteLine("Opção inválida.");
+            }
+        }
+        ExibirOpcoesDoMenu();
+    }
+}
